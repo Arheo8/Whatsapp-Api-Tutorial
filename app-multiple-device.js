7a09a55a332bed161ecd378ee193778fa9a0f185
Arheo8 <78500348+Arheo8@users.noreply.github.com>
2021-10-27 18:08:24 +0530

    keeping-alive

--- app-multiple-device.js
+++ app-multiple-device.js
@@ -13,6 +13,30 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+function startKeepAlive() {
+  setInterval(function() {
+      var options = {
+          host: 'https://arheo-whatsapp-api.herokuapp.com',
+          port: 80,
+          path: '/'
+      };
+      http.get(options, function(res) {
+          res.on('data', function(chunk) {
+              try {
+                  // optional logging... disable after it's working
+                  console.log("HEROKU RESPONSE: " + chunk);
+              } catch (err) {
+                  console.log(err.message);
+              }
+          });
+      }).on('error', function(err) {
+          console.log("Error: " + err.message);
+      });
+  }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
+
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
